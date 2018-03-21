ada1b115c32b07b0359c1eae66f8248c587f1149
Steven Huang <steven.charles.huang@gmail.com>
2018-03-20 21:58:21 -0400

    commit 3 completed - made square divs loop

--- pixart.js
+++ pixart.js
@@ -48,12 +48,27 @@ colorTextIn.addEventListener('keyup', function (evt) {
 // ### Commit 3
 // * Create 20 divs of the "square" class and append them to the body
 //   * **Hint**: use `.appendChild()`
-
+////////////////////////////////////////////////////////////////////////
+// Create 20 divs of square class
+function addSquareDivs () {
+  for (let i = 0; i <= 20; i++) {
+    // Make Square Divs
+    let squareDivs = document.createElement('div');
+    squareDivs.setAttribute('class', 'square');
+    // Append to Body
+    document.body.appendChild(squareDivs);
+  }
+}
+addSquareDivs();
+////////////////////////////////////////////////////////////////////////
 // ### Commit 4
 // * Add functionality so that when I click on each "square", it changes
 //  the color of that individual square to "green"
 //   * **Hint**: either add the event listener while creating the
 // squares, or listen for events on the `body` element
+////////////////////////////////////////////////////////////////////////
+
+
 
 // ### Commit 5
 // * Modify your code so that when I click on each "square", it changes
